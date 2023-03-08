04fa4a9f4e70d21ea7cc26f6c3684c6e5e38a747
Szymon Borda <szymon.borda@deployed.pl>
2023-03-08 16:20:06 +0100

    add EdgeInput

--- src/pages/Projekt1/Projekt1.tsx
+++ src/pages/Projekt1/Projekt1.tsx
@@ -1,7 +1,19 @@
+import { useState } from 'react';
+import EdgeInput from './EdgeInput';
+
+export interface Edge {
+  name: string;
+  from: number;
+  to: number;
+  time: number;
+}
+
 function Projekt1() {
+  const [edges, setEdges] = useState<Edge[]>([]);
   return (
     <div className="projekt1">
       <h1>Strona pod projekt 1</h1>
+      <EdgeInput edges={edges} setEdges={setEdges} />
     </div>
   );
 }
