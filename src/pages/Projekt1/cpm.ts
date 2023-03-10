433dce25560041fd74fcc66ff42828860970d062
Szymon Borda <szymon.borda@gmail.com>
2023-03-10 21:19:04 +0100

    fix ts and lint errors in calculateCPM

--- src/pages/Projekt1/cpm.ts
+++ src/pages/Projekt1/cpm.ts
@@ -1,34 +1,33 @@
 import { Action } from './types';
 
-export default function calculateCPM(actions: Action[]) {
-  for(let i in actions) {
-    if(actions[i].predecessors.length === 0) {
+export default function calculateCPM(actionsRef: Action[]) {
+  const actions: Action[] = structuredClone(actionsRef);
+  for (let i = 0; i < actions.length; i += 1) {
+    if (actions[i].predecessors.length === 0) {
       actions[i].ES = 0;
       actions[i].EF = actions[i].time;
-    }
-    else if(actions[i].predecessors.length === 1) {
+    } else if (actions[i].predecessors.length === 1) {
       // search for predecessor and extract time
-      for(let j in actions) {
-        if(actions[i].predecessors[0] === actions[j].name) {
-          actions[i].ES = actions[j].time + actions[j].ES;
-          actions[i].EF = actions[i].ES + actions[i].time;
+      for (let j = 0; j < actions.length; j += 1) {
+        if (actions[i].predecessors[0] === actions[j].name) {
+          actions[i].ES = actions[j].time + actions[j].ES!;
+          actions[i].EF = actions[i].ES! + actions[i].time;
           break;
         }
       }
-    }
-    else {
+    } else {
       // search for predecessors and calculate every possible ES
       // from gathered results select the maximum
-      let arr: number[] = []
-      for(let a in actions[i].predecessors) {
-        for(let b in actions) {
-          if(actions[i].predecessors[a] === actions[b].name) {
-            arr.push(actions[b].time + actions[b].ES);
+      const arr: number[] = [];
+      for (let a = 0; a < actions[i].predecessors.length; a += 1) {
+        for (let b = 0; b < actions.length; b += 1) {
+          if (actions[i].predecessors[a] === actions[b].name) {
+            arr.push(actions[b].time + actions[b].ES!);
           }
         }
       }
       actions[i].ES = Math.max(...arr);
-      actions[i].EF = actions[i].ES + actions[i].time;
+      actions[i].EF = actions[i].ES! + actions[i].time;
     }
   }
   return actions;
