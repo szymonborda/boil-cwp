f5aa46614608c1a2abc3a18d58c4a5b84c1e1d96
Jarczk <36330112+Jarczk@users.noreply.github.com>
2023-03-27 09:03:36 +0200

    GUI improvement (#4)
    * Add CSS and modified to material-panel style
    
    * Add start date and changes in styles
    
    * Repair most of ESLint errors
    
    * Revert .eslintc edit
    
    * improve gui
    
    ---------
    
    Co-authored-by: Szymon Borda <szymon.borda@deployed.pl>

--- src/pages/Projekt1/Gantt.tsx
+++ src/pages/Projekt1/Gantt.tsx
@@ -7,21 +7,25 @@ import { Action } from './types';
 
 interface GanttProps {
   actions: Action[];
+  startDate?: string;
 }
 
-function Gantt({ actions }: GanttProps) {
+function Gantt({ actions, startDate }: GanttProps) {
+  const start = startDate ? new Date(startDate).getDate() : new Date().getDate();
   const tasks: Task[] = actions.map((action) => ({
     id: action.name,
     type: 'task' as const,
     name: action.name,
-    start: new Date(new Date().setDate(new Date().getDate() + action.ES! || 0)),
-    end: new Date(new Date().setDate(new Date().getDate() + action.EF! || 0)),
+    start: new Date(new Date(new Date().setDate(start + action.ES! || 0)).setHours(0, 0, 0, 0)),
+    end: new Date(new Date(new Date().setDate(start + action.EF! || 0)).setHours(0, 0, 0, 0)),
     dependencies: action.predecessors,
     progress: 100,
   }));
   return (
     <GanttTaskReact
       tasks={tasks}
+      listCellWidth=""
+      locale="pl"
     />
   );
 }
--- src/pages/Projekt1/Projekt1.tsx
+++ src/pages/Projekt1/Projekt1.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { Action } from './types';
 import ActionInput from './ActionInput';
 import ActionOutput from './ActionOutput';
@@ -9,21 +9,46 @@ import calculateCPM from './cpm';
 function Projekt1() {
   const [actions, setActions] = useState<Action[]>(examples[2]);
   const [isCalculated, setIsCalculated] = useState(false);
+  const startDate = useRef<HTMLInputElement>(null);
   const calculate = () => {
     setActions(calculateCPM(actions));
     setIsCalculated(true);
   };
   return (
     <div className="projekt1">
-      <h1>Strona pod projekt 1</h1>
-      <ActionInput actions={actions} setActions={setActions} setIsCalculated={setIsCalculated} />
-      <button onClick={calculate} type="button">Oblicz</button>
-      {isCalculated ? (
-        <>
-          <ActionOutput actions={actions} />
-          <Gantt actions={actions} />
-        </>
-      ) : null}
+      <div className="header">
+        <h1>Critical Path Method</h1>
+        <h4>Projekt 1</h4>
+      </div>
+      <div className="panelContainer">
+        <div className="panel">
+          <div className="title">Wprowadzanie danych</div>
+          <div className="subtitle">
+            Data rozpoczęcia:
+            {' '}
+            <input type="date" name="startDate" ref={startDate} onChange={() => setIsCalculated(false)} style={{ margin: '10px' }} />
+          </div>
+          <ActionInput
+            actions={actions}
+            setActions={setActions}
+            setIsCalculated={setIsCalculated}
+          />
+          <button onClick={calculate} type="button" className="buttonFilled" style={{ width: '100%', marginTop: '10px' }}>Oblicz</button>
+        </div>
+
+        {isCalculated ? (
+          <>
+            <div className="panel">
+              <div className="title">Wyniki obliczeń</div>
+              <ActionOutput actions={actions} />
+            </div>
+            <div className="panel">
+              <div className="title">Wykres Gantt&apos;a</div>
+              <Gantt actions={actions} startDate={startDate.current?.value} />
+            </div>
+          </>
+        ) : null}
+      </div>
     </div>
   );
 }
